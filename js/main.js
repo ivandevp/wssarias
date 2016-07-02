66f6a2ce10cd4a50f413338144b03072569187fd
Iván Medina Díaz <medinadiazivan@gmail.com>
2016-07-02 12:38:21 +0000

    Image toggle transition

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 $(window).load(function() {
     var isColombia = false;
     setInterval(function() {
-        $("#img1").fadeToggle();
-        $("#img2").fadeToggle();
+        $("#img1").fadeToggle(1000);
+        $("#img2").fadeToggle(1000);
         if (isColombia) {
-            $("body").css("background-image", "url('img/small-background_PSV.jpg')");
+            $("body").css({
+                "background-image": "url('img/small-background_PSV.jpg')",
+                "transition": "opacity 1s ease-in-out"
+            });
             isColombia = false;
         } else {
-            $("body").css("background-image", "url('img/small-background_Colombia.jpg')");
+            $("body").css({
+                "background-image": "url('img/small-background_Colombia.jpg')",
+                "transition": "opacity 1s ease-in-out"
+            });
             isColombia = true;
         }
     }, 3000);
